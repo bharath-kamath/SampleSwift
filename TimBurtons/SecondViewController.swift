2e2561c68d23a19118a8d48cbbe7a6123490b730
Bharath Kamath <bharathkamath@Bharaths-MacBook-Air.local>
2018-02-02 18:00:52 -0500

    Initial Commit

---
+++ TimBurtons/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TimBurtons
+//
+//  Created by Bharath Kamath on 02/02/18.
+//  Copyright © 2018 Bharath Kamath. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
